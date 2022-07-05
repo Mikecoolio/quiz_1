edd8b3324220d7dc5faea045f26216db46ae7fc1
mikecoolio <mikepoon604@gmail.com>
2022-07-05 10:11:06 -0700

    Finished Part 1: Application Skeleton, will now load bootstrap into project

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+
+const methodOverride = require('method-override');
+
+app.use(methodOverride((req, res) => {
+  if (req.body && req.body.__method) {
+    const method = req.body.__method
+    return method
+  }
+}))
+
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req,res, next) => {
+  const username = req.cookies.username || '';
+
+  res.locals.username = username;
+  next();
+})
+
+const PORT = 5245
+const HOST = 'localhost'
+app.listen(PORT, HOST, () => {
+  console.log(`The Cluckr server is listening at ${HOST}:${PORT}`);
+})
 
 // Part 1: Application Skeleton
 // Value: 9%
