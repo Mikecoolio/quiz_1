aaa74fb2b39f0fe6304991493fc87bebe561bd66
mikecoolio <mikepoon604@gmail.com>
2022-07-05 09:56:56 -0700

    added all required dependacies, wrote header and footer partials

---
+++ index.js
@@ -0,0 +1,9 @@
+// Part 1: Application Skeleton
+// Value: 9%
+
+// Build an Express app with the following:
+
+// Logging middleware installed and setup.
+// Nodemon tool installed as a development dependency and made useable as a NPM script.
+// Static assets middleware setup.
+
